fc361e6ba328132f55b5e2f34e51ec54d234a901
Alex Cojocaru <lex.cojocaru@gmail.com>
2024-07-25 22:19:20 +0300

    correction

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -21,7 +21,7 @@ function fetchItems(shop) {
   db.collection(blacksmithshop).get().then((querySnapshot) => {
       let itemsList = '';
       querySnapshot.forEach((doc) => {
-          itemsList += `<li>${doc.data().name} - ${doc.data().Value} ${doc.data().Currency}</li>`;
+          itemsList += `<li>${doc.data().Name} - ${doc.data().Value} ${doc.data().Currency}</li>`;
       });
       document.getElementById('items').innerHTML = `<ul>${itemsList}</ul>`;
   });
