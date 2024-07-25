02e23055110e544fa864c2b920434d60940cbc3f
Alex Cojocaru <lex.cojocaru@gmail.com>
2024-07-25 21:37:53 +0300

    Add db function

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -10,6 +10,18 @@ const firebaseConfig = {
   appId: "1:937158092711:web:a03bee6d47f0534fd9f882"
 };
 
+// Example function to fetch items for a specific shop
+function fetchItems(shop) {
+  db.collection(blacksmithshop).get().then((querySnapshot) => {
+      let itemsList = '';
+      querySnapshot.forEach((doc) => {
+          itemsList += `<li>${doc.data().name} - ${doc.data().Value} coins</li>`;
+      });
+      document.getElementById('items').innerHTML = `<ul>${itemsList}</ul>`;
+  });
+}
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
